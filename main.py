e53b0d4ab33a8a7f2a3a6557af2859216a0684fb
Shibaditya Deb <debshibaditya144@gmail.com>
2025-06-25 19:47:25 +0530

    buggs fixed of wikipedia search and save file function

--- main.py
+++ main.py
@@ -4,22 +4,20 @@ from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
-from langchain.agents import create_tool_calling_agent , AgentExecutor
-from tools import search_tool , wiki_tool, save_tool
-
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
 
 load_dotenv()
 
 class ResearchResponse(BaseModel):
-    topic:str
-    summary:str
-    sources:list[str]
-    tools_used:list[str]
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+    
 
-
-
-llm=ChatAnthropic(model='claude-3-5-sonnet-20241022')
-parser=PydanticOutputParser(pydantic_object=ResearchResponse)
+llm = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
 
 prompt = ChatPromptTemplate.from_messages(
     [
@@ -37,21 +35,19 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 ).partial(format_instructions=parser.get_format_instructions())
 
-tools=[search_tool,wiki_tool,save_tool]
-agent=create_tool_calling_agent(
+tools = [search_tool, wiki_tool, save_tool]
+agent = create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
-    tools=[]
+    tools=tools
 )
 
-agent_executor=AgentExecutor(agent=agent,tools=tools,verbose=True)
-query=input("What can I help you in research?")
-raw_response=agent_executor.invoke({"query":query})
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query = input("What can i help you research? ")
+raw_response = agent_executor.invoke({"query": query})
 
 try:
-
-    structured_response=parser.parse(raw_response.get('output')[0]['text'])
-    print(structured_response.topic)
+    structured_response = parser.parse(raw_response.get("output")[0]["text"])
+    print(structured_response)
 except Exception as e:
-    print("Error parsing response",e)
-
+    print("Error parsing response", e, "Raw Response - ", raw_response)
--- tools.py
+++ tools.py
@@ -1,6 +1,6 @@
-from langchain_community.tools import WikipediaQueryRun , DuckDuckGoSearchRun
+from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
 from langchain_community.utilities import WikipediaAPIWrapper
-from langchain.tools import Tool 
+from langchain.tools import Tool
 from datetime import datetime
 
 def save_to_txt(data: str, filename: str = "research_output.txt"):
@@ -18,14 +18,12 @@ save_tool = Tool(
     description="Saves structured research data to a text file.",
 )
 
-
-search=DuckDuckGoSearchRun()
-search_tool=Tool(
+search = DuckDuckGoSearchRun()
+search_tool = Tool(
     name="search",
     func=search.run,
     description="Search the web for information",
 )
 
-api_wrapper=WikipediaAPIWrapper(top_k_results=2,doc_content_chars_max=100)
-
-wiki_tool=WikipediaQueryRun(api_wrapper=api_wrapper)
+api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
+wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
